54d1d6c382268c4d5fed5fd30467c12033455617
Daniel Medina <daniel.sm@gmail.com>
2021-03-17 11:11:15 -0300

    added options and fixed multiple connections being open

--- mongoUtil.js
+++ mongoUtil.js
@@ -4,30 +4,62 @@ var _client;
 var _db;
 var _bucket;
 
+var calls = 0;
+var interval;
+
+checkIdle = function(){
+    // If no calls made to connection pool in the last interval,
+    // drop connection and stop checking idle.
+    if (calls){
+        calls = 0;
+    }
+    else{
+        
+        if(_client) _client.close();
+        _client = undefined;
+        _db = undefined;
+        _bucket = undefined;
+        clearInterval(interval);
+    }
+}
+
 module.exports = {
 
   getClient: function(globalOptions) {
+        calls++; //Increase counter
         var options = globalOptions || {};
+        var mongoOptions = options.mongoOptions || {};
         
+        // MongoDB URI
         _.defaults(options, {
             uri: 'mongodb://localhost:27017/mydatabase'
         });
+
+        // Idle time to drop MongoClient
         _.defaults(options, {
-            mongoOptions: {}
+            maxIdleConnTime: 10000 // in ms, default 10s
         });
+
+        // Mongo Options
+        _.defaults(mongoOptions, {
+            useUnifiedTopology: true,
+            poolSize: 10
+        });
+
         
-        options.mongoOptions = Object.assign({ useNewUrlParser: true, useUnifiedTopology: true, poolSize: 200 }, options.mongoOptions);
 
         return new Promise(function(resolve,reject){
-            if(_client) resolve(_client);
-            MongoClient.connect (options.uri, options.mongoOptions).then((client) => {
+            if(_client) return resolve(_client);
+            MongoClient.connect (options.uri, mongoOptions).then((client) => {
                 _client = client;
                 _db  = client.db();
                 _bucket = new GridFSBucket(_db);
+
                 resolve(client);
             }).catch((err) => {
                 reject(err);
             })
+            interval = setInterval(checkIdle,options.maxIdleConnTime); //Check idle every 10 secs.
         })
     },
 
